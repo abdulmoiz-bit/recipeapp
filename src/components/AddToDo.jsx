79f7b5331aa2605e9d7ef9eb96600c6e51d50f6e
Researcher <70377003+abdulmoiz-bit@users.noreply.github.com>
2023-11-14 03:45:53 +0500

    more

--- src/components/AddToDo.jsx
+++ src/components/AddToDo.jsx
@@ -17,7 +17,7 @@ const AddToDo = ({ todos, handleDelete, addList }) => {
   const [messageId] = useState(uniqueId('message-'));
 
   const hiddenFileInput = useRef(null);
- // const [image, setImage] = useState("");
+  // const [image, setImage] = useState("");
   const [title, setTitle] = useState("");
   const [desc, setDesc] = useState("");
   const [category, setCategory] = useState("");
@@ -35,9 +35,9 @@ const AddToDo = ({ todos, handleDelete, addList }) => {
       setCategory("");
     }
   }
- const handleImageChange = () => {
+  const handleImageChange = () => {
 
- }
+  }
   return (
     <>
       <div className='main-recipe'>
@@ -48,25 +48,25 @@ const AddToDo = ({ todos, handleDelete, addList }) => {
             <div className='popup'>
               <form className='popup__content' action="/action_page.php" onSubmit={addTodo}>
                 <input className='popup__file' type="file" name="filename"
-                 onChange={handleImageChange}
-                 ref={hiddenFileInput}
-                 /> <br />
-                <h1 style={{marginTop: "2rem"}}>Title</h1>
+                  onChange={handleImageChange}
+                  ref={hiddenFileInput}
+                /> <br />
+                <h1 style={{ marginTop: "2rem" }}> Title </h1>
                 <input id={messageId} className='popup__title' type="text" placeholder='Title' value={title} onChange={(e) => setTitle(e.target.value)} /> <br />
-                <h1 style={{marginTop: "2rem"}}>Description</h1>
-                <textarea className='popup__desc' type="text" placeholder='Desc' value={desc} onChange={(e) => setDesc(e.target.value)}/ > <br/>
+                <h1 style={{ marginTop: "2rem" }}>Description</h1>
+                <textarea className='popup__desc' type="text" placeholder='Desc' value={desc} onChange={(e) => setDesc(e.target.value)} /> <br />
                 <div className='popup__dropdown'>
-                <select name="foods" id="food" value={category} onChange={e => setCategory(e.target.value)}>
-                  <option value="traditional">Choose a Category </option>
-                  <option value="fast food">Fast Food</option>
-                  <option value="sea food">Sea Food</option>
-                  <option value="soup">Soup</option>
-                  <option value="drinks">Drinks</option>
-                </select>
+                  <select name="foods" id="food" value={category} onChange={e => setCategory(e.target.value)}>
+                    <option value="traditional">Choose a Category </option>
+                    <option value="fast food">Fast Food</option>
+                    <option value="sea food">Sea Food</option>
+                    <option value="soup">Soup</option>
+                    <option value="drinks">Drinks</option>
+                  </select>
                 </div>
                 <br />
                 <button className='popup__button' onClick={addTodo}>Submit</button>
-                <button style={{marginLeft: "1rem"}}  className='popup__button' onClick={() => showModal()}>Close</button>
+                <button style={{ marginLeft: "1rem" }} className='popup__button' onClick={() => showModal()}>Close</button>
               </form>
             </div>
           )
@@ -83,6 +83,6 @@ const AddToDo = ({ todos, handleDelete, addList }) => {
 
     </>
   )
-}
+};
 
 export default AddToDo;
